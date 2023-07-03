d83cb2fed03cc6a7d2c92dd08e9e29c41a6b6ca8
Max Kofler <kofler.max.dev@gmail.com>
2023-07-03 19:29:05 +0000

    mount: Improve error messaging

--- src/mount.rs
+++ src/mount.rs
@@ -1,4 +1,4 @@
-use std::io::Error;
+use crate::StdIOErrorExt;
 use std::path::Path;
 use sys_mount::*;
 
@@ -14,11 +14,11 @@ pub fn mount_overlay(
     work: &Path,
     upper: &Path,
     merged: &Path,
-) -> Result<UnmountDrop<Mount>, Error> {
-    std::fs::create_dir_all(lower)?;
-    std::fs::create_dir_all(upper)?;
-    std::fs::create_dir_all(work)?;
-    std::fs::create_dir_all(merged)?;
+) -> Result<UnmountDrop<Mount>, std::io::Error> {
+    std::fs::create_dir_all(lower).err_prepend("When ensuring lower directory")?;
+    std::fs::create_dir_all(upper).err_prepend("When ensuring upper directory")?;
+    std::fs::create_dir_all(work).err_prepend("When ensuring work directory")?;
+    std::fs::create_dir_all(merged).err_prepend("When ensuring merged directory")?;
 
     let lower_s = lower.to_string_lossy();
     let work_s = work.to_string_lossy();
@@ -31,15 +31,11 @@ pub fn mount_overlay(
         &merged.to_string_lossy()
     );
 
-    let mount_result = Mount::builder()
+    Mount::builder()
         .fstype("overlay")
         .data(&data)
-        .mount("overlay", merged);
-
-    match mount_result {
-        Ok(mount) => Ok(mount.into_unmount_drop(UnmountFlags::DETACH)),
-        Err(e) => Err(e),
-    }
+        .mount_autodrop("overlay", merged, UnmountFlags::DETACH)
+        .err_prepend("When mounting overlayfs")
 }
 
 /// Mounts the following Linux virtual kernel filesystems from `source` to `destination`:
@@ -47,7 +43,10 @@ pub fn mount_overlay(
 /// # Arguments
 /// * `source` - The source to take the mounts from (usually `/`)
 /// * `destination` - The destination to mount the vkfs into
-pub fn mount_vkfs(source: &Path, destination: &Path) -> Result<Vec<UnmountDrop<Mount>>, Error> {
+pub fn mount_vkfs(
+    source: &Path,
+    destination: &Path,
+) -> Result<Vec<UnmountDrop<Mount>>, std::io::Error> {
     // Create quick handlers
     let src_dev = source.join("dev");
 
@@ -58,13 +57,12 @@ pub fn mount_vkfs(source: &Path, destination: &Path) -> Result<Vec<UnmountDrop<M
     let dest_tmp = destination.join("tmp");
 
     // Ensure the target directories exist
-    std::fs::create_dir_all(&destination)?;
+    std::fs::create_dir_all(&destination).err_prepend("When ensuring destination")?;
 
-    std::fs::create_dir_all(&dest_dev)?;
-    std::fs::create_dir_all(&dest_dev_pts)?;
-    std::fs::create_dir_all(&dest_proc)?;
-    std::fs::create_dir_all(&dest_sys)?;
-    std::fs::create_dir_all(&dest_tmp)?;
+    std::fs::create_dir_all(&dest_dev).err_prepend("When ensuring /dev")?;
+    std::fs::create_dir_all(&dest_proc).err_prepend("When ensuring /proc")?;
+    std::fs::create_dir_all(&dest_sys).err_prepend("When ensuring /sys")?;
+    std::fs::create_dir_all(&dest_tmp).err_prepend("When ensuring /tmp")?;
 
     let flags = UnmountFlags::FORCE;
 
@@ -76,35 +74,41 @@ pub fn mount_vkfs(source: &Path, destination: &Path) -> Result<Vec<UnmountDrop<M
     );
     let mount_dev = Mount::builder()
         .flags(MountFlags::BIND)
-        .mount_autodrop(&src_dev, &dest_dev, flags)?;
+        .mount_autodrop(&src_dev, &dest_dev, flags)
+        .err_prepend("When mounting vkfs dev /dev")?;
 
     // /dev/pts
+    std::fs::create_dir_all(&dest_dev_pts).err_prepend("When ensuring /dev/pts")?;
     info!(
         "[vkfs] Mounting devpts to {}",
         &dest_dev_pts.to_string_lossy()
     );
-    let mount_dev_pts =
-        Mount::builder()
-            .fstype("devpts")
-            .mount_autodrop("devpts", &dest_dev_pts, flags)?;
+    let mount_dev_pts = Mount::builder()
+        .fstype("devpts")
+        .mount_autodrop("devpts", &dest_dev_pts, flags)
+        .err_prepend("When mounting vkfs devpts /dev/pts")?;
 
     // /proc
     info!("[vkfs] Mounting proc to {}", &dest_proc.to_string_lossy());
     let mount_proc = Mount::builder()
         .fstype("proc")
-        .mount_autodrop("proc", &dest_proc, flags)?;
+        .mount_autodrop("proc", &dest_proc, flags)
+        .err_prepend("When mounting vkfs proc /proc")?;
 
     // /sys
     info!("[vkfs] Mounting sysfs to {}", &dest_sys.to_string_lossy());
     let mount_sys = Mount::builder()
         .fstype("sysfs")
-        .mount_autodrop("sysfs", &dest_sys, flags)?;
+        .mount_autodrop("sysfs", &dest_sys, flags)
+        .err_prepend("When mounting vkfs sysfs /sys")?;
 
     // /tmp
     info!("[vkfs] Mounting tmpfs to {}", &dest_tmp.to_string_lossy());
     let mount_tmp = Mount::builder()
         .fstype("tmpfs")
-        .mount_autodrop("tmpfs", &dest_tmp, flags)?;
+        .mount_autodrop("tmpfs", &dest_tmp, flags)
+        .err_prepend("When mounting tmpfs")
+        .err_prepend("When mounting vkfs tmpfs /tmp")?;
 
     // Push the UnmountDrops to a vector
     let mut res: Vec<UnmountDrop<Mount>> = Vec::new();
